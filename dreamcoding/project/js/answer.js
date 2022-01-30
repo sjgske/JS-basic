3e82ed5dec4e269f3259e05ceb0b3b06dcc383c5
danheon kim <eksgjs3847@naver.com>
2022-01-30 20:18:01 +0900

    fix a bug

--- dreamcoding/project/js/answer.js
+++ dreamcoding/project/js/answer.js
@@ -1,6 +1,6 @@
 // Fetch the items from the JSON file
 function loadItems() {
-  return fetch('../data/data.json')
+  return fetch('data/data.json')
     .then(response => response.json())
     .then(json => json.items);
 }
