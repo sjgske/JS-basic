8e6faabd7b5a8ce965e2b0160117b94eb89f4e76
danheon kim <eksgjs3847@naver.com>
2022-03-01 04:09:18 +0900

    Improve structure of the code

--- kimbug-javascript-101/agreement/app.js
+++ kimbug-javascript-101/agreement/app.js
@@ -1,55 +1,54 @@
-const form = document.querySelector('.signup-form')
-const checkAll = document.querySelector('.form-check-all input')
-const checkboxes = document.querySelectorAll('.form-group input')
-const submitButton = document.querySelector('button')
+const form = document.querySelector(".signup-form");
+const checkAll = document.querySelector("#checkAll");
+const checkboxes = document.querySelectorAll(".form-group input");
+const submitButton = document.querySelector("button");
 
 const agreements = {
-  termsOfService: false,
-  privacyPolicy: false,
-  allowPromotions: false,
-}
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
-})
-
-checkboxes.forEach(function (item) {
-  item.addEventListener('input', toggleCheckbox)
-})
+	termsOfService: false,
+	privacyPolicy: false,
+	allowPromotions: false,
+};
 
 function toggleCheckbox(e) {
-  const { checked, id } = e.currentTarget
-  if (!checked) {
-    checkAll.checked = false
-  }
-  agreements[id] = checked
-  this.parentNode.classList.toggle('active')
-  toggleSubmitButton()
+	// 체크되어있으면 checkbox의 아이디에 해당하는 agreements의 property 값이 true로 바뀌어야됨
+	const { checked, id } = e.currentTarget;
+	if (!checked) {
+		checkAll.checked = false;
+	}
+	agreements[id] = checked;
+	toggleSubmitButton();
 }
 
 function toggleSubmitButton() {
-  const { termsOfService, privacyPolicy } = agreements
-  if (termsOfService && privacyPolicy) {
-    submitButton.removeAttribute('disabled')
-  } else {
-    submitButton.setAttribute('disabled', '')
-  }
+	const { termsOfService, privacyPolicy } = agreements;
+	if (termsOfService && privacyPolicy) {
+		submitButton.disabled = false;
+	} else {
+		submitButton.disabled = true;
+	}
 }
 
-checkAll.addEventListener('click', function (e) {
-  const { checked } = e.currentTarget
-  if (checked) {
-    checkboxes.forEach(function (item) {
-      item.checked = true
-      agreements[item.id] = true
-      item.parentNode.classList.add('active')
-    })
-  } else {
-    checkboxes.forEach(function (item) {
-      item.checked = false
-      agreements[item.id] = false
-      item.parentNode.classList.remove('active')
-    })
-  }
-  toggleSubmitButton()
-})
+// form 제출 시 새로고침 방지
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+});
+
+checkboxes.forEach(item => {
+	item.addEventListener("input", toggleCheckbox);
+});
+
+checkAll.addEventListener("click", e => {
+	const { checked } = e.currentTarget;
+	if (checked) {
+		checkboxes.forEach(item => {
+			item.checked = true;
+			agreements[item.id] = true;
+		});
+	} else {
+		checkboxes.forEach(item => {
+			item.checked = false;
+			agreements[item.id] = false;
+		});
+	}
+	toggleSubmitButton();
+});
