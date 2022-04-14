69eb294a57eb97965655d1e2a6800c7c8a78ee82
danheon kim <eksgjs3847@naver.com>
2022-04-14 22:50:33 +0900

    Implement tab menu ui (vanilla js)

---
+++ tab-button/index.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".tab-content");
+
+function tabMenu(e, tabId) {
+  console.log(tabId);
+  let activeTab = document.getElementById(tabId);
+
+  btns.forEach((el) => el.classList.remove("active"));
+  contents.forEach((el) => el.classList.remove("active"));
+
+  e.target.classList.add("active");
+  activeTab.classList.add("active");
+}
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", (e) => {
+    tabMenu(e, `tab-${i + 1}`);
+  });
+}
